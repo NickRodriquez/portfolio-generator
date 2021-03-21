a70849e0d3c585f27d53f2471ea40fc9b305ef01
Nick Elijah Rodriquez <73867466+NickRodriquez@users.noreply.github.com>
2021-03-21 16:25:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-var message = 'Hello Node!';
-
-var sum = 5 + 3;
-
-console.log(message);
-console.log(sum);
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
